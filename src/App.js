9789afed64d3cf4f5c30e9a2f2b49c8def2e9bf5
Marlon Jackson <jwjackson31@gmail.com>
2025-12-17 16:59:48 -0700

    Completed Project using Bulma library for styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import ProfileCard from './ProfileCard'
+import 'bulma/css/bulma.css'
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
+
 
 function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p>Personal Digital Assistants</p>
+                </div>
+            </section>
 
-            <ProfileCard title="Alexa" handle="@alex99" />
-            <ProfileCard title="Cortana" handle="@cortan32" />
-            <ProfileCard title="Siri" handle="@siri01" />
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Alexa"
+                                handle="@alex99"
+                                image={AlexaImage}
+                                description="Alexa was created by Amazon and helps you buy things"
+                            />
+
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Cortana"
+                                handle="@cortan32"
+                                image={CortanaImage}
+                                description="Cortana was made by Microsoft. Who knows what it does?"
+                            />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard
+                                title="Siri"
+                                handle="@siri01"
+                                image={SiriImage}
+                                description="Siri was made by Apple and is being phased out"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+
 
         </div>
     );
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle }) {
+function ProfileCard({ title, handle, image, description }) {
     // const title = props.title;
     // const handle = props.handle;
 
@@ -7,9 +7,20 @@ function ProfileCard({ title, handle }) {
 
 
     return (
-        <div>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
         </div>
     )
 }
